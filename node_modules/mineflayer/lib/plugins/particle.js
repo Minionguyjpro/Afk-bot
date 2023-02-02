826985f8d767f62d071d9dd6ce345f25b9eec948
Minionguyjpro <66115754+Minionguyjpro@users.noreply.github.com>
2023-02-02 14:49:13 +0100

    Delete node_modules directory

--- node_modules/mineflayer/lib/plugins/particle.js
+++
@@ -1,9 +0,0 @@
-module.exports = inject
-
-function inject (bot, { version }) {
-  const Particle = require('../particle')(bot.registry)
-
-  bot._client.on('world_particles', (packet) => {
-    bot.emit('particle', Particle.fromNetwork(packet))
-  })
-}
